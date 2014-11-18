da6fd6835d4a01486e17cdce2e93b348484bc48a
Emanuel Evans <emanuel.evans@gmail.com>
2014-11-17 19:17:11 -0800

    tdi role

---
+++ roles/tdi.rb
@@ -0,0 +1,9 @@
+name 'tdi'
+run_list ['recipe[irc]', 'recipe[git]', 'recipe[chruby::system]']
+default_attributes 'chruby' => {
+	'rubies' => {
+		'1.9.3-p392' => false,
+		'1.9.3-p429' => true
+	},
+	'default' => '1.9.3-p429'
+	} 
